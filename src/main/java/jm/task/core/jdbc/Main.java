60bd610ec32600d37614a4c5b2940901ade0cd6c
andrey0000347 <alebedev02@gmail.com>
2025-05-22 22:54:34 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -19,11 +19,18 @@ public class Main {
 
         userServiceImpl.createUsersTable();
 
-        // Сохранение пользователей
+        // Сохранение пользователей и вывод сообщений
         userServiceImpl.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        System.out.println("User с именем " + user1.getName() + " добавлен в базу данных.");
+
         userServiceImpl.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        System.out.println("User с именем " + user2.getName() + " добавлен в базу данных.");
+
         userServiceImpl.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        System.out.println("User с именем " + user3.getName() + " добавлен в базу данных.");
+
         userServiceImpl.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        System.out.println("User с именем " + user4.getName() + " добавлен в базу данных.");
 
         // Получение всех пользователей
         System.out.println("Все пользователи:");
